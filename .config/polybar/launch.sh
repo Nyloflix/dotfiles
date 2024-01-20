50767efe5485639f5e08de41553383c917a76c9d
Benedikt Hauer <mailbox@hautschi.at>
2024-01-20 12:20:30 +0100

    forgot to include .config :P

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+#polybar bar &
+
+
+# Show on all Monitors
+# (requires
+#	monitor = ${env:MONITOR:}
+# in launch.sh script)
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload bar &
+fi
+
+
+echo "Polybar launched..."
